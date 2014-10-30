5eab3724bcb0191b91be92247b8694ea1c58cf40
Bradley Bossard <bradleybossard@gmail.com>
2014-10-30 12:51:08 -0700

    Initial layout generated from LibGDX tool

---
+++ android/src/com/bradleybossard/overlaygame/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.bradleybossard.overlaygame.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.bradleybossard.overlaygame.MyGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGame(), config);
+	}
+}
---
+++ desktop/src/com/bradleybossard/overlaygame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bradleybossard.overlaygame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bradleybossard.overlaygame.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
